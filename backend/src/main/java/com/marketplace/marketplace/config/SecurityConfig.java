fc2da0a63145e488d1f58b9e3bdcaaff40d71833
Moustafa Daebis <mostafa.dabis@gmail.com>
2026-09-16 18:26:07 +0300

    add swagger

--- backend/src/main/java/com/marketplace/marketplace/config/SecurityConfig.java
+++ backend/src/main/java/com/marketplace/marketplace/config/SecurityConfig.java
@@ -40,7 +40,11 @@ public class SecurityConfig {
             "/add-item",
             "/css/**",
             "/js/**",
-            "/images/**"
+            "/images/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html/",
+            "/v3/api-docs/**",
+            "/v3/api-docs/",
     };
 
     @Bean
